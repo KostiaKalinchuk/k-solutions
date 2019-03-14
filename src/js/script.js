4eb7919d7ae59dd49f53060ae379c4f11e670099
Your Name <9leon1@gmail.com>
2019-03-14 19:40:03 +0200

    finished

--- src/js/script.js
+++ src/js/script.js
@@ -2,19 +2,19 @@
 @@include('partials/slick.js');
 @@include('partials/main.js');
 
-function view(idDiv){
-//                var divs = document.getElementsByTagName('div');
+
+function view(idDiv) {
     var divs = document.getElementsByClassName('payment');
-    for(var i = 0; i < divs.length; i++){
-        if(divs[i].id == idDiv){
+    for (var i = 0; i < divs.length; i++) {
+        if (divs[i].id == idDiv) {
             divs[i].style.display = 'block';
             continue;
         }
         divs[i].style.display = 'none';
     }
     var div = document.getElementsByClassName('payment-method');
-    for(var i = 0; i < div.length; i++){
-        if(div[i].id == idDiv){
+    for (var i = 0; i < div.length; i++) {
+        if (div[i].id == idDiv) {
             div[i].style.display = 'flex';
             continue;
         }
@@ -34,39 +34,32 @@ $(document).ready(function () {
 
 function sendAjaxForm(result_form, ajax_form, url) {
     $.ajax({
-        url: url, //url страницы (action_ajax_form.php)
-        type: "POST", //метод отправки
-        dataType: "html", //формат данных
-        data: $("#" + ajax_form).serialize(),  // Сеарилизуем объект
-        success: function (response) { //Данные отправлены успешно
+        url: url,
+        type: "POST",
+        dataType: "html",
+        data: $("#" + ajax_form).serialize(),
+        success: function (response) {
             result = $.parseJSON(response);
-
-
-            console.log (result);
-
-
+            // console.log(result);
             var form = $('<form/>', {
-                method : 'POST',
-                action : 'https://pay.piastrix.com/ru/pay',
-                character_set : 'UTF-8',
+                method: 'POST',
+                action: 'https://pay.piastrix.com/ru/pay',
+                character_set: 'UTF-8',
             }).appendTo('#result_form');
-            form.append('<input type="text" name="amount" value="'+result.amount+'">');
-            form.append('<input type="text" name="currency" value="'+result.currency+'">');
-            form.append('<input type="text" name="shop_id" value = "'+result.shop_id+'"/>');
-            form.append('<input type="text" name="sign" value="'+result.sign+'">');
-            form.append('<input type="text" name="payway" value="'+result.payway+'">');
-            form.append('<input type="text" name="shop_order_id" value="'+result.shop_order_id+'">');
-            form.append('<input type="text" name="description" value="'+result.description+'">');
+            form.append('<input type="text" name="amount" value="' + result.amount + '">');
+            form.append('<input type="text" name="currency" value="' + result.currency + '">');
+            form.append('<input type="text" name="shop_id" value = "' + result.shop_id + '"/>');
+            form.append('<input type="text" name="sign" value="' + result.sign + '">');
+            form.append('<input type="text" name="payway" value="' + result.payway + '">');
+            form.append('<input type="text" name="shop_order_id" value="' + result.shop_order_id + '">');
+            form.append('<input type="text" name="description" value="' + result.description + '">');
             form.append('<input type="submit" value="Отправить запрос"/>');
-
-
         },
-        error: function (response) { // Данные не отправлены
+        error: function (response) {
             $('#result_form').html('Ошибка. Данные не отправлены.');
         }
     });
 }
-
 
 var demo = document.getElementsByClassName('demonstration')[0];
 demo.onclick = function () {
@@ -85,12 +78,3 @@ close.onclick = function () {
     var b = document.getElementById('bg_layer');
     b.setAttribute("style", "visibility:hidden");
 };
-
-
-// $('#bg_layer').hide();
-// $('#btn').click(function(){
-//     $('#bg_layer').show();
-// });
-// $('.close').click(function(){
-//     $('#bg_layer').hide();
-// });
